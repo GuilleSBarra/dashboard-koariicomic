9a72df8130049ad2189e624ae3fcab2179f8a996
Lucia <lucia.aznar1987@gmail.com>
2022-01-21 15:30:06 -0300

    Subo actualizaciones en la vista de usuarios

--- src/components/UltimoUsuarioCreado.js
+++ src/components/UltimoUsuarioCreado.js
@@ -1,43 +1,49 @@
-import React, {Component} from "react";
+/* eslint-disable jsx-a11y/alt-text */
+import React, { Component } from "react";
 class UltimoUsuarioCreado extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lastUser: [],
+      url: "https://serene-beyond-45379.herokuapp.com/http://koaricomic.herokuapp.com/api/users",
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state={
-            ultimoUsuario: [],
-            url: "https://serene-beyond-45379.herokuapp.com/http://koaricomic.herokuapp.com/api/users"
-        }
-    }
+  apiCall(url, consecuencia) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => consecuencia(data))
+      .catch((error) => console.log(error));
+  }
 
-    apiCall(url, consecuencia){
-        fetch(url)
-            .then(response => response.json())
-            .then (data => consecuencia(data))
-            .catch(error => console.log(error))
-    }
+  componentDidMount() {
+    console.log("-----");
+    this.apiCall(this.state.url, this.mostrarInfo);
+  }
 
-    componentDidMount() {
-        console.log("-----")
-        this.apiCall(this.state.url, this.mostrarInfo )
-        
-      }
-
-      mostrarInfo = (data) => {
-          console.log(data)
-          this.setState({
-            ultimoUsuario: data.users[data.users.length -1].name
-              })
-      }
-      render() {
-        
-        return (
-          <div>
-              <p className="ver1">El ultimo usuario que de dio de alta es: {this.state.ultimoUsuario}</p>
-          </div>
-        );
-        
-    }
-
+  mostrarInfo = (data) => {
+    console.log(data);
+    this.setState({
+      lastUser: data.users[data.users.length - 1],
+    });
+  };
+  render() {
+    return (
+      <section className="conteinerTableProducts">
+        <p className="verUsus">El ultimo usuario que de dio de alta es:</p>
+        <table className="tableProducts">
+          <tbody>
+            <tr>
+              <td>{this.state.lastUser.id}</td>
+              <td>{this.state.lastUser.name}</td>
+              <td>{this.state.lastUser.email}</td>
+            </tr>
+          </tbody>
+        </table>
+        <img src={`https://koaricomic.herokuapp.com/${this.state.lastUser.detail}`}/>
+      </section>
+    );
+  }
 }
 
 export default UltimoUsuarioCreado;
